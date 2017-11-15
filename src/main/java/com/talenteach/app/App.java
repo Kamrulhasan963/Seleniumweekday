3f8fa78bad280f2fa6d50c443352aeff83cc2b49
Kamrul Hasan <atmkhasan@gmail.com>
2017-11-15 14:59:45 -0400

    first commit

---
+++ src/main/java/com/talenteach/app/App.java
@@ -0,0 +1,13 @@
+package com.talenteach.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
